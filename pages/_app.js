af5afe56bba90dc2ce38a4af2b517fdc1fb123eb
chatbox9321 <85362220+chatbox9321@users.noreply.github.com>
2022-05-14 17:35:49 +0530

    Seventh commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 import { createClient } from "contentful"
 import RecipeCard from '../components/Recipe';
 
