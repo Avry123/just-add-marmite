74bcfe8989714b1ee3529fc6ee9f4c5c47cf8d60
chatbox9321 <85362220+chatbox9321@users.noreply.github.com>
2022-05-15 11:12:53 +0530

    Twelvth commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:next/recommended"
+        "plugin:react/recommended"
     ],
     "parserOptions": {
         "ecmaFeatures": {
